a841574faadd63de7c207487cf25550eb4874736
MadMakZa <immakza@gmail.com>
2020-12-27 23:56:00 +0300

    sensorManager and animation

--- app/src/main/java/com/example/compass/MainActivity.kt
+++ app/src/main/java/com/example/compass/MainActivity.kt
@@ -1,11 +1,53 @@
 package com.example.compass
 
+import android.content.Context
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.animation.Animation
+import android.view.animation.RotateAnimation
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), SensorEventListener {
+
+    var manager: SensorManager? = null
+    var current_degree:Int = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //менеджер управления сенсором
+        manager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+    }
+
+    override fun onResume() {
+        super.onResume()
+        manager?.registerListener(this, manager?.getDefaultSensor(Sensor.TYPE_ORIENTATION), SensorManager.SENSOR_DELAY_GAME)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        manager?.unregisterListener(this)
+    }
+
+    //точность сенсора
+    override fun onSensorChanged(p0: Sensor?, p1: Int) {
+
+    }
+    //данные от сенсора
+    override fun onAccuracyChanged(p0: SensorEvent?) {
+        val degree:Int = p0?.values?.get(0)?.toInt()!!
+        tvDegree.text = degree.toString() //передача показателей в текствью
+        //анимация
+        val rotationAnim = RotateAnimation(current_degree.toFloat(), (-degree).toFloat(),
+            Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f)
+        rotationAnim.duration = 210
+        rotationAnim.fillAfter = true
+        //изначальное положение
+        current_degree = -degree
+
     }
 }
